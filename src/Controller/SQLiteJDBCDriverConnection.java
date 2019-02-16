cd6e9a5fc72b08ff3a471fd8d6929521f04bbc9f
Cho <won_suk_cho@dlsu.edu.ph>
2019-02-16 21:44:02 +0800

    fixed password and hashing

--- src/Controller/SQLiteJDBCDriverConnection.java
+++ src/Controller/SQLiteJDBCDriverConnection.java
@@ -2,6 +2,9 @@ package Controller;
 
 
 import Model.User;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -65,7 +68,6 @@ public class SQLiteJDBCDriverConnection {
     }
     
     public void addUser(String username, String password) {
-        username = username.toLowerCase();
         String sql = "INSERT INTO users(username,password) VALUES('" + username + "','" + password + "')";
         
         try (Connection conn = DriverManager.getConnection(driverURL);
@@ -82,7 +84,36 @@ public class SQLiteJDBCDriverConnection {
     }
     
         public void addUser(String username, String password, int role) {
-        username = username.toLowerCase();
+        
+        //link to how to do SHA-512 encryption (https://www.geeksforgeeks.org/sha-512-hash-in-java/)
+        
+        try { 
+            // getInstance() method is called with algorithm SHA-512 
+            MessageDigest md = MessageDigest.getInstance("SHA-512"); 
+  
+            // digest() method is called 
+            // to calculate message digest of the input string 
+            // returned as array of byte 
+            byte[] messageDigest = md.digest(password.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+  
+            // Add preceding 0s to make it 32 bit 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            }
+            password = hashtext;
+        } 
+  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+            
         String sql = "INSERT INTO users(username,password,role) VALUES('" + username + "','" + password + "','" + role + "')";
         
         try (Connection conn = DriverManager.getConnection(driverURL);
--- src/Model/User.java
+++ src/Model/User.java
@@ -1,9 +1,5 @@
 package Model;
 
-import java.math.BigInteger; 
-import java.security.MessageDigest; 
-import java.security.NoSuchAlgorithmException; 
-
 public class User {
     private int id;
     private String username;
@@ -42,34 +38,7 @@ public class User {
     }
 
     public void setPassword(String password) {
-        //link to how to do SHA-512 encryption (https://www.geeksforgeeks.org/sha-512-hash-in-java/)
-        
-        try { 
-            // getInstance() method is called with algorithm SHA-512 
-            MessageDigest md = MessageDigest.getInstance("SHA-512"); 
-  
-            // digest() method is called 
-            // to calculate message digest of the input string 
-            // returned as array of byte 
-            byte[] messageDigest = md.digest(password.getBytes()); 
-  
-            // Convert byte array into signum representation 
-            BigInteger no = new BigInteger(1, messageDigest); 
-  
-            // Convert message digest into hex value 
-            String hashtext = no.toString(16); 
-  
-            // Add preceding 0s to make it 32 bit 
-            while (hashtext.length() < 32) { 
-                hashtext = "0" + hashtext; 
-            }
-            this.password = hashtext;
-        } 
-  
-        // For specifying wrong message digest algorithms 
-        catch (NoSuchAlgorithmException e) { 
-            throw new RuntimeException(e); 
-        } 
+        this.password = password;
     }
 
     public int getRole() {
